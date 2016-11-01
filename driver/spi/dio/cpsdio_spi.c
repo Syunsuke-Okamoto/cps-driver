ea59ebc2408f0b5a75b4a8b881f37c7c45738275
okamoto@contec.jp <okamoto@contec.jp>
2016-11-01 17:25:53 +0900

    1, Nov, 2016 : cpsdio_spi modify (1) modify cpsdio_spi driver.

--- driver/spi/dio/cpsdio_spi.c
+++ driver/spi/dio/cpsdio_spi.c
@@ -7,6 +7,10 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
+ * Ver 0.0.1 : first make.
+ * Ver 0.0.2 : digital filter added.
+ * Ver.0.0.3 : 
  */
 
 #include <linux/init.h>
@@ -20,11 +24,53 @@
 #include <linux/cdev.h>
 #include <asm/uaccess.h>
 
-#ifdef CONFIG_CONPROSYS_SDK
- #include "../include/cpsdio.h"
-#else
- #include "../../../include/cpsdio.h"
-#endif
+#include "cpsdio_spidevdata.h"
+#include "cpsdio.h"
+#include "cps_mc341.h"
+
+
+/**
+	@~English
+	@param address : offset address
+	@param type : byte access or word access
+	@param value : value
+	@~Japanese
+	@brief CPS-MC341-DSXのSPI ICへのSPI送信用パケットを作成します。
+	@param address : オフセットアドレス
+	@param type : バイトアクセスか　ワードアクセス
+	@param value : 値
+**/
+#define CPS_SET_MC341_DS1X_SPI_WRITE_PACKET( address, type, value ) \
+CPS_SPI_SET_PACKET(	\
+			PAGE_CPS_MC341_DSX_DIO,	\
+			CPS_CATEGORY_DIO,	\
+			address, \
+			CPS_SPI_ACCESS_WRITE, \
+			type, \
+			value \
+)
+
+/**
+	@~English
+	@param address : offset address
+	@param type : byte access or word access
+	@param value : value
+	@~Japanese
+	@brief CPS-MC341-DSXのSPI ICへのSPI受信用パケットを作成します。
+	@param address : オフセットアドレス
+	@param type : バイトアクセスか　ワードアクセス
+	@param value : 値
+**/
+#define CPS_SET_MC341_DS1X_SPI_READ_PACKET( address, type ) \
+		CPS_SPI_SET_PACKET(\
+			PAGE_CPS_MC341_DSX_DIO,\
+			CPS_CATEGORY_DIO,\
+			address,\
+			CPS_SPI_ACCESS_READ,\
+			type,	\
+			(0x0000)	\
+)
+
 
 #define CPSDIOX_PACKET_SIZE	4
 #define CPSDIO_SPI_CHANNELS	1
@@ -32,15 +78,15 @@
 #define CPSDIO_SPI_MINOR 0
 #define CPSDIO_SPI_NUM_DEVS 1
 #define CPSDIO_SPI_DRIVER_NAME	"cpsdio"
-#define DRV_VERSION "0.0.1"
+#define DRV_VERSION "0.0.4"
 #define max_speed 6000000
 
 /*	Define the name of the device and sysfs class name*/
 static struct cdev cpsdio_spi_cdev;
 static struct class *cpsdio_spi_class = NULL;
 
-static int cpsdio_spi_major = 0;
-static int cpsdio_spi_minor = 0;
+static int cpsdio_spi_major = 0;	///< SPI Major ID
+static int cpsdio_spi_minor = 0;	///< SPI Minor ID
 
 struct CPSDIO_SPI_drvdata {
 	struct spi_device *spi;
@@ -67,34 +113,153 @@ static int cpsdio_open(struct inode *inode, struct file *filp) {
 	return 0;
 }
 
-static int cpsdio_write_output(struct spi_device *spi, const void *buf, size_t len) {
+/**
+	@~English
+	@brief SPI Write function with CONTEC FPGA
+	@param spi : SPI structure
+	@param address : offset address
+	@param size : data size
+	@param value : value
+	@return true : 0, false : otherwise
+	@~Japanese
+	@brief CONTECのFPGAに対するSPI書き込み関数です。
+	@param spi : SPI構造体
+	@param address : オフセットアドレス
+	@param size : サイズ
+	@param value : 値
+	@return 成功: 0 , 失敗: それ以外
+**/
+static long cpsdio_spi_write(struct spi_device *spi, int address, int size ,unsigned short value ) {
 	int status;
+	int access_type;
+	CPS_SPI_FORMAT tx;
 
-	status = spi_write(spi, buf, len);
+	// サイズ選択
+	switch( size ){
+	case 1 :
+		access_type = CPS_SPI_ACCESS_TYPE_BYTE;
+		break;
+	case 2 :
+		access_type = CPS_SPI_ACCESS_TYPE_WORD;
+		break;
+	default :
+		pr_err("FAILURE: %s failed \n", __FUNCTION__);
+		return -ENODEV;
+	}
+
+	tx.value = CPS_SET_MC341_DS1X_SPI_WRITE_PACKET(
+			address,
+			access_type,
+			value
+	);
+
+	status = spi_write(spi, (const void *)&tx, sizeof(tx));
+
 	if (status < 0) {
 		pr_err("FAILURE: %s failed \n", __FUNCTION__);
 		return -ENODEV;
-	} else {
-		return 0;
 	}
+
+	return 0;
+}
+
+/**
+	@~English
+	@brief SPI Read function with CONTEC FPGA
+	@param spi : SPI structure
+	@param address : offset address
+	@param size : data size
+	@param value : value
+	@return true : 0, false : otherwise
+	@~Japanese
+	@brief CONTECのFPGAに対するSPI読み込み関数です。
+	@param spi : SPI構造体
+	@param address : オフセットアドレス
+	@param size : サイズ
+	@param value : 値
+	@return 成功: 0 , 失敗: それ以外
+**/
+static long cpsdio_spi_read(struct spi_device *spi, int address, int size ,unsigned short *value ) {
+	int status;
+	int access_type;
+	CPS_SPI_FORMAT rx;
+
+	switch( size ){
+	case 1 :
+		access_type = CPS_SPI_ACCESS_TYPE_BYTE;
+		break;
+	case 2 :
+		access_type = CPS_SPI_ACCESS_TYPE_WORD;
+		break;
+	default :
+		pr_err("FAILURE: %s failed \n", __FUNCTION__);
+		return -ENODEV;
+	}
+
+	rx.value = CPS_SET_MC341_DS1X_SPI_READ_PACKET(
+			address,
+			access_type
+	);
+
+	status = spi_write_then_read(
+			spi,
+			&(rx.COM_VAL.Frame.command[0]),
+			sizeof(rx.COM_VAL.Frame.command),
+			&(rx.COM_VAL.value[0]),
+			sizeof(rx.COM_VAL.value)
+	);
+
+	if (status < 0) {
+		pr_err("FAILURE: %s failed \n", __FUNCTION__);
+		return -ENODEV;
+	}
+	*value = rx.value;
+	return 0;
+}
+
+/**
+	@~English
+	@brief This function set Digital Output Data.
+	@param spi : SPI structure
+	@param port : Port Number
+	@param size : data size
+	@param value : value
+	@return true : 0, false : otherwise
+	@~Japanese
+	@brief デジタル出力のデータを設定する関数
+	@param spi : SPI structure
+	@param port : ポート番号
+	@param size : データサイズ
+	@param value : 値
+	@return 成功: 0 , 失敗: それ以外
+**/
+static long cpsdio_write_output(struct spi_device *spi, int port, int size ,unsigned short value ) {
+
+	int address = OFFSET_OUTPUT0_CPS_MC341_DSX + port;
+	return cpsdio_spi_write(spi, address, size ,value );
 }
 EXPORT_SYMBOL_GPL(cpsdio_write_output);
 
-static int cpsdio_read_input(struct spi_device *spi, unsigned char buf[], size_t len) {
-	int status;
-	unsigned char rx_cmd[]={0x00, 0x00};
-	unsigned char rx_val[]={0x00, 0x00};
+/**
+	@~English
+	@brief This function get Digital Input Data.
+	@param spi : SPI structure
+	@param port : Port Number
+	@param size : data size
+	@param value : value
+	@return true : 0, false : otherwise
+	@~Japanese
+	@brief デジタル入力のデータを取得する関数
+	@param spi : SPI structure
+	@param port : ポート番号
+	@param size : データサイズ
+	@param value : 値
+	@return 成功: 0 , 失敗: それ以外
+**/
+static long cpsdio_read_input(struct spi_device *spi, int port, int size ,unsigned short *value ) {
 
-	rx_cmd[0] = buf[0];
-	rx_cmd[1] = buf[1];
-
-	status = spi_write_then_read(spi,rx_cmd, sizeof(rx_cmd), rx_val, sizeof(rx_val));
-	if (status < 0) {
-		pr_err("FAILURE: %s failed \n", __FUNCTION__);
-		return -ENODEV;
-	} else {
-		return rx_val[1];
-	}
+	int address = OFFSET_INPUT0_CPS_MC341_DSX + port;
+	return cpsdio_spi_read(spi, address, size ,value );
 }
 
 static int cpsdio_close(struct inode *inode, struct file *filp) {
@@ -102,37 +267,65 @@ static int cpsdio_close(struct inode *inode, struct file *filp) {
 	return 0;
 }
 
-static long cpsdio_get_digital_filter( struct spi_device *spi, unsigned char buf[], size_t len )
+/**
+	@~English
+	@brief This function get Digital Filter.
+	@param spi : SPI structure
+	@param value : digital filter value.
+	@return true : 0, false : otherwise
+	@~Japanese
+	@brief デジタル入力のデジタルフィルタを取得する関数
+	@param spi : SPI構造体
+	@param value : デジタルフィルタの取得値
+	@return 成功: 0 , 失敗: それ以外
+**/
+static long cpsdio_get_digital_filter( struct spi_device *spi, unsigned char *value )
 {
-	int status;
-	unsigned char rx_cmd[]={0x00, 0x00};
-	unsigned char rx_val[]={0x00, 0x00};
+	long lRet = 0;
+	int address = OFFSET_DIGITALFILTER_CPS_MC341_DSX;
+	unsigned short valw;
 
-	rx_cmd[0] = buf[0];
-	rx_cmd[1] = buf[1];
+	lRet = cpsdio_spi_read(spi, address, 1 ,&valw );
 
-	status = spi_write_then_read(spi,rx_cmd, sizeof(rx_cmd), rx_val, sizeof(rx_val));
-	if (status < 0) {
-		pr_err("FAILURE: %s failed \n", __FUNCTION__);
-		return -ENODEV;
-	} else {
-		return rx_val[0];
-	}
+	if( lRet ) return lRet;
+
+	*value = valw;
+	return 0;
 }
 
-static long cpsdio_set_digital_filter( struct spi_device *spi, const void *buf, size_t len )
+/**
+	@~English
+	@brief This function set Digital Filter.
+	@param spi : SPI structure
+	@param value : digital filter value.
+	@return true : 0, false : otherwise
+	@~Japanese
+	@brief デジタル入力のデジタルフィルタを設定する関数
+	@param spi : SPI構造体
+	@param value : デジタルフィルタの取得値
+	@return 成功: 0 , 失敗: それ以外
+**/
+static long cpsdio_set_digital_filter( struct spi_device *spi, unsigned char value )
 {
-	int status;
+	int address = OFFSET_DIGITALFILTER_CPS_MC341_DSX;
 
-	status = spi_write(spi, buf, len);
-	if (status < 0) {
-		pr_err("FAILURE: %s failed \n", __FUNCTION__);
-		return -ENODEV;
-	} else {
-		return 0;
-	}
+	return cpsdio_spi_write(spi, address, 1 ,(unsigned short)value );
 }
 
+/**
+	@~English
+	@brief cpsdio_ioctl
+	@param filp : struct file pointer
+	@param cmd : iocontrol command
+	@param arg : argument
+	@return Success 0, Failed:otherwise 0. (see errno.h)
+	@~Japanese
+	@brief cpsdio_ioctl
+	@param filp : file構造体ポインタ
+	@param cmd : I/O コントロールコマンド
+	@param arg : 引数
+	@return 成功:0 , 失敗:0以外 (errno.h参照)
+**/
 static long cpsdio_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
 	struct device *dev;
 	struct spi_device *spi;
@@ -140,13 +333,16 @@ static long cpsdio_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	struct CPSDIO_SPI_drvdata *data;
 	char str[128];
 
-	unsigned char tx[]={0x00, 0x00, 0x00, 0x00};
-	unsigned char rx[]={0x00, 0x00, 0x00, 0x00};
+	long lRet = 0;
+	unsigned char valb = 0;
+	unsigned short valw = 0;
+
 	struct cpsdio_ioctl_arg ioc;
 
 	master = spi_busnum_to_master(CPSDIO_SPI_info.bus_num);
 	snprintf(str, sizeof(str), "%s.%u", dev_name(&master->dev),
 			CPSDIO_SPI_info.chip_select);
+
 	dev = bus_find_device_by_name(&spi_bus_type, NULL, str);
 	spi = to_spi_device(dev);
 	data = (struct CPSDIO_SPI_drvdata *)spi_get_drvdata(spi);
@@ -162,12 +358,12 @@ static long cpsdio_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				return -EFAULT;
 			}
 			mutex_lock( &data->lock );
-			rx[1] = (unsigned char)ioc.port;
-			ioc.port = ioc.port >> 8;
-			rx[0] = (unsigned char)ioc.port;
-			ioc.val = (unsigned int)cpsdio_read_input(spi,rx,sizeof(&rx));
-
+			lRet = cpsdio_read_input(spi, ioc.port, 1, &valw);
+			ioc.val = valw;
 			mutex_unlock( &data->lock );
+
+			if( lRet )	return lRet;
+
 			if( copy_to_user( (int __user *)arg, &ioc, sizeof(ioc) ) ){
 				return -EFAULT;
 			}
@@ -180,13 +376,11 @@ static long cpsdio_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				return -EFAULT;
 			}
 			mutex_lock( &data->lock );
-			tx[1] = (unsigned char)ioc.port;
-			ioc.port = ioc.port >> 8;
-			tx[0] = (unsigned char)ioc.port;
-			tx[2] = tx[3] = (unsigned char)ioc.val;
-			cpsdio_write_output(spi, (const void *)&tx, sizeof(&tx));
-
+			lRet = cpsdio_write_output(spi, ioc.port, 1, ioc.val);
 			mutex_unlock( &data->lock );
+
+			if( lRet ) return lRet;
+
 			break;
 		case IOCTL_CPSDIO_SET_FILTER:
 			if(!access_ok(VERITY_READ, (void __user *)arg, _IOC_SIZE(cmd) ) ) {
@@ -197,13 +391,11 @@ static long cpsdio_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				return -EFAULT;
 			}
 			mutex_lock(&data->lock);
-
-			tx[0] = (unsigned char)0x01;
-			tx[1] = (unsigned char)0x8C;
-			tx[2] = (unsigned char)ioc.val;
-			cpsdio_set_digital_filter(spi,(const void *)&tx, sizeof(&tx));
+			lRet = cpsdio_set_digital_filter(spi, (unsigned char)ioc.val);
 			mutex_unlock(&data->lock);
+			if( lRet ) return lRet;
 			break;
+
 		case IOCTL_CPSDIO_GET_FILTER:
 			if(!access_ok(VERITY_WRITE, (void __user *)arg, _IOC_SIZE(cmd) ) ) {
 				return -EFAULT;
@@ -212,10 +404,12 @@ static long cpsdio_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				return -EFAULT;
 			}
 			mutex_lock(&data->lock);
-			rx[0] = (unsigned char)0x01;
-			rx[1] = (unsigned char)0x84;
-			ioc.val = cpsdio_get_digital_filter(spi,rx,sizeof(&rx));
+			lRet = cpsdio_get_digital_filter(spi,&valb);
+			ioc.val = valb;
 			mutex_unlock(&data->lock);
+
+			if( lRet ) return lRet;
+
 			if (copy_to_user((int __user *) arg, &ioc, sizeof(ioc))) {
 				return -EFAULT;
 			}
@@ -309,12 +503,12 @@ static int CPSDIO_SPI_probe(struct spi_device *spi) {
 
 static int CPSDIO_SPI_remove(struct spi_device *spi) {
 	struct CPSDIO_SPI_drvdata *data;
-	unsigned char tx[]={0x01, 0x2C, 0x00, 0x00};
+//	unsigned char tx[]={0x01, 0x2C, 0x00, 0x00};
 
 	data = (struct CPSDIO_SPI_drvdata *) spi_get_drvdata(spi);
 
 	/*	DIO LED initialization processing*/
-	cpsdio_write_output(spi, (const void *)&tx, sizeof(&tx));
+	cpsdio_write_output(spi, 0, 2, 0x0000 );
 
 	kfree(data);
 	pr_info( "cpsdio_spi driver removed\n");
@@ -323,17 +517,19 @@ static int CPSDIO_SPI_remove(struct spi_device *spi) {
 }
 
 static struct spi_device_id CPSDIO_SPI_id[] = {
-		{ "CPSDIO_SPI", 0 },
-		{ }, };
+		{ "CPS_MC341_DS1X", 0 },
+		{ },
+};
 MODULE_DEVICE_TABLE(spi, CPSDIO_SPI_id);
 
 static struct spi_driver CPSDIO_SPI_driver = {
 		.driver = {
 				.name =	CPSDIO_SPI_DRIVER_NAME,
-				.owner = THIS_MODULE, },
-				.id_table =	CPSDIO_SPI_id,
-				.probe = CPSDIO_SPI_probe,
-				.remove = CPSDIO_SPI_remove,
+				.owner = THIS_MODULE,
+		},
+		.id_table =	CPSDIO_SPI_id,
+		.probe = CPSDIO_SPI_probe,
+		.remove = CPSDIO_SPI_remove,
 };
 
 static void spi_remove_device(struct spi_master *master, unsigned int cs) {
@@ -365,7 +561,7 @@ static int CPSDIO_SPI_init(void) {
 		pr_err( "spi_busnum_to_master returned NULL\n");
 		goto init_err;
 	}
-/*	Remove because spidev2.0 in the initial state is occupying*/
+/*	Remove because spidev 2.0 in the initial state is occupying*/
 	spi_remove_device(master, CPSDIO_SPI_info.chip_select);
 	spi_device = spi_new_device(master, &CPSDIO_SPI_info);
 	if (!spi_device) {
