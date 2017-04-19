5fa923061d5cb88a00c3c296c1f27669c2b3fef6
okamoto@contec.jp <okamoto@contec.jp>
2017-04-19 20:38:01 +0900

    19, Apr, 2017: CpsAioQueryDeviceName Test

---
+++ sample/aio/queryDeviceName.c
@@ -0,0 +1,56 @@
+/* 
+-- queryDeviceName.c
+-- Copyright (c) 2016 Syunsuke Okamoto
+--
+-- This software is released under the MIT License.
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+-- 
+*/
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
+#include "../../include/libcpsaio.h"
+
+int main(){
+
+	int cnt;
+	int iRet;
+	char DevName[16];
+	char Dev[32];
+
+	cnt = 0;
+	do{
+		iRet = ContecCpsAioQueryDeviceName(cnt, DevName,Dev );
+		if( iRet == 0 ){
+			printf("Device %s DeviceName : %s\r\n", Dev, DevName);
+		}
+		cnt ++;
+	}while(iRet == 0);
+
+
+	return 0;
+}
+
